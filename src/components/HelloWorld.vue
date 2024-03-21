dd59cf8d33fc1a8c38aeb0349cd917636e101fd7
Volkov Alexander <volkov.525@gmail.com>
2024-03-21 13:21:08 +0300

    before tranfer

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+defineProps<{ msg: string }>();
+
+const count = ref(0);
+</script>
+
+<template lang="pug">
+h1 {{ msg }}
+p lakjsl
+
+</template>
+
+<style scoped lang="scss">
+.read-the-docs {
+  color: #888;
+}
+p {
+  color: red;
+}
+</style>
