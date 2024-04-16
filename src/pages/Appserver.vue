76697abe655973ac872ee994a3731dd6ea3d152f
Volkov Alexander <volkov.525@gmail.com>
2024-04-16 17:13:15 +0300

    add scroll

--- src/pages/Appserver.vue
+++ src/pages/Appserver.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { ref, computed, reactive } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { tree } from '@/stores/confTree1'
 
@@ -11,11 +11,30 @@ const current = computed(() => {
 	const curr = flat.find(item => item.id == route.params.id)
 	return curr
 })
-// console.log(current.value)
+
+const list = reactive([
+	// { id: 0, label: 'Лицензия', selected: false },
+	{ id: 1, field: 'bd', label: 'Базы данных', selected: false },
+	{ id: 2, field: 'common', label: 'Общие настройки', selected: false },
+	{ id: 3, field: 'dost', label: 'Управление доступом', selected: false },
+	{
+		id: 4,
+		field: 'auth',
+		label: 'Расширенная аутентификация',
+		selected: false,
+	},
+])
+
+const selected = ref()
+const select = (e: any) => {
+	selected.value = e.label
+	// router.replace('/database/AGSupport#' + e.field)
+	document.getElementById(e.field)?.scrollIntoView({ behavior: 'smooth' })
+}
 </script>
 
 <template lang="pug">
-q-page(padding)
+q-page
 	.bread
 		q-breadcrumbs
 			q-breadcrumbs-el(v-for="item in route.meta.bread" :label="item.label" :icon="item.icon" @click="router.back")
@@ -25,7 +44,45 @@ q-page(padding)
 			q-btn(unelevated color="primary" label="Применить" size="md") 
 
 	.container
-		h2 Hello
-</template>
+		.grid
+			q-list.left(dense)
+				q-item(clickable v-for="item in list" :key="item.id" :class="{selected: selected == item.label}" @click="select(item)")
+					q-item-section
+						q-item-label {{ item.label }}
+			q-scroll-area.right
+				.q-ml-lg
+					#bd.commonzg Базы данных
+					p(v-for="n in 20") lakjsdlkjals Laы длфоЫдДФДлфыоДло   лдфОЫдлфоЫДл 
+					#common.commonzg Общие настройки
+					p(v-for="n in 20") lakjsdlkjals Laы длфоЫдДФДлфыоДло   лдфОЫдлфоЫДл 
+					#dost.commonzg Управление доступом
+					p(v-for="n in 20") lakjsdlkjals Laы длфоЫдДФДлфыоДло   лдфОЫдлфоЫДл 
+					#auth.commonzg Расширенная аутентификация
+					p(v-for="n in 20") lakjsdlkjals Laы длфоЫдДФДлфыоДло   лдфОЫдлфоЫДл 
+	</template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.grid {
+	display: grid;
+	grid-template-columns: auto 1fr;
+	justify-items: start;
+	align-items: start;
+	column-gap: 3rem;
+	row-gap: 0.5rem;
+	// background: green;
+}
+.right,
+.left {
+	height: calc(100vh - 165px);
+	width: 100%;
+	// background: pink;
+}
+.container {
+	margin: 1rem 2rem;
+}
+.selected {
+	color: $blue-10;
+	border: 1px solid hsl(221 41% 73% / 1);
+	background: var(--tree-selection);
+}
+</style>
