61b8848378e89d182a73c08b9023b90fb5676114
Volkov Alexander <volkov.525@gmail.com>
2024-05-14 16:43:05 +0300

    add wrapper for webclient

---
+++ src/components/tree/WebClientInside.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+// import AppServerCommon from '@/components/tree/AppServerCommon.vue'
+// import AppServerDostup from '@/components/tree/AppServerDostup.vue'
+// import AppServerAuth from '@/components/tree/AppServerAuth.vue'
+// import Database from '@/components/tree/Database.vue'
+</script>
+
+<template lang="pug">
+#connect.commonzg Подключение к серверу приложений
+#common.commonzg Общие настройки
+#cache.commonzg Кэширование
+#system.commonzg Система
+#crypto.commonzg Облачная подпись КриптоПро
+#grid.commonzg Грид
+#locale.commonzg Локализации
+#cards.commonzg Виды карточек
+#experiment.commonzg Экспериментальные функции
+</template>
+
+<style scoped lang="scss">
+.align {
+	margin-left: 2rem;
+}
+</style>
--- src/components/tree/Zagl.vue
+++ src/components/tree/Zagl.vue
@@ -2,6 +2,7 @@
 import { reactive } from 'vue'
 import BreadCrumbs from '@/components/tree/BreadCrumbs.vue'
 import TreeList from '@/components/tree/TreeList.vue'
+import WebClientInside from '@/components/tree/WebClientInside.vue'
 
 const props = defineProps({
 	id: {
@@ -9,19 +10,6 @@ const props = defineProps({
 		required: true,
 	},
 })
-
-const list = reactive([
-	// { id: 0, label: 'Лицензия', selected: false },
-	{ id: 1, field: 'bd', label: 'Базы данных', selected: false },
-	{ id: 2, field: 'common', label: 'Общие настройки', selected: false },
-	{ id: 3, field: 'dost', label: 'Управление доступом', selected: false },
-	{
-		id: 4,
-		field: 'auth',
-		label: 'Расширенная аутентификация',
-		selected: false,
-	},
-])
 </script>
 
 <template lang="pug">
@@ -30,9 +18,11 @@ q-page
 	.container
 		.grid
 			TreeList
+
 			q-scroll-area.right
 				.q-ml-lg
 					.confzag  {{ props.id[1]}}
+					WebClientInside
 </template>
 
 <style scoped lang="scss">
