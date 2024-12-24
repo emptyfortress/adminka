898927aad523e1f440a9fd3e651a34bfaebdfa9c
Volkov Alexander <volkov.525@gmail.com>
2024-12-24 12:16:23 +0300

    redone site

--- src/components/tree/WebClientCommon.vue
+++ src/components/tree/WebClientCommon.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import MyField from '@/components/common/MyField.vue'
-import MyRadioGroup3 from '@/components/common/MyRadioGroup3.vue'
-import PropertyTab from '../setupcomponent/PropertyTab.vue';
 
 const Device = ref([
 	{
@@ -59,26 +57,115 @@ const Protokol = ref([
 	{
 		id: 0,
 		main: '0',
-		label: 'Интервал автоматического обновления сессии, ч',
-		descr: 'Продление действия cookies при входе пользователя в Web-клиент.',
+		label: 'Использовать http',
+		descr: 'Подключение web-клиента по незащищенному протоколу',
 		readonly: false,
+		checkbox: true,
+		check: true
 	},
-
+	{
+		id: 1,
+		main: '0',
+		label: 'Использовать https',
+		descr: 'Подключение web-клиента по безопасному протоколу',
+		readonly: false,
+		checkbox: true,
+		check: true
+	},
+])
+const Protokol1 = ref([
+	{
+		id: 0,
+		main: '',
+		label: 'Сайт',
+		descr: 'Адрес подключения к Web-клиенту',
+		readonly: false,
+		type: 'text'
+	},
+	{
+		id: 1,
+		main: '',
+		label: 'Порт',
+		descr: 'Порт подключения к Web-клиенту',
+		readonly: false,
+		type: 'number'
+	},
+])
+const Protokol2 = ref([
+	{
+		id: 0,
+		main: '',
+		label: 'Сайт',
+		descr: 'Адрес подключения к Web-клиенту',
+		readonly: false,
+		type: 'text'
+	},
+	{
+		id: 1,
+		main: '',
+		label: 'Порт',
+		descr: 'Порт подключения к Web-клиенту',
+		readonly: false,
+		type: 'number'
+	},
+	{
+		id: 2,
+		main: '',
+		label: 'Сертификат',
+		descr: 'Путь к штампу сертификата',
+		readonly: false,
+		type: 'text'
+	},
+	{
+		id: 3,
+		main: '',
+		label: 'Пароль',
+		descr: 'Пароль серитфиката',
+		readonly: false,
+		type: 'text'
+	},
 ])
 </script>
 
 <template lang="pug">
 q-form
-	.section Настройка протокола
+	.section Выбор протокола
 	q-list
-		MyRadioGroup3(
+		MyField(
 			v-model:main="item.main" 
 			v-for="item in Protokol" 
 			:key="item.id" 
 			:label="item.label" 
 			:descr="item.descr" 
 			:readonly="item.readonly"
+			:check="item.check"
+			:checkbox="item.checkbox"
 			)
+
+	.section Настройка протокола http
+	q-list
+		MyField(
+			v-model:main="item.main" 
+			v-for="item in Protokol1" 
+			:key="item.id" 
+			:label="item.label" 
+			:descr="item.descr" 
+			:readonly="item.readonly"
+			:type='item.type'
+			)
+
+	.section Настройка протокола https
+	q-list
+		MyField(
+			v-model:main="item.main" 
+			v-for="item in Protokol2" 
+			:key="item.id" 
+			:label="item.label" 
+			:descr="item.descr" 
+			:readonly="item.readonly"
+			:type='item.type'
+			)
+
 
 	.section Пользовательские cookie
 	q-list
