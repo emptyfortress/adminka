c2728e1f5b00a81a31544ff0aadee17ab130043f
Volkov Alexander <volkov.525@gmail.com>
2025-06-09 10:33:09 +0300

    workflow new

--- src/components/tree/WorkflowConnect.vue
+++ src/components/tree/WorkflowConnect.vue
@@ -1,17 +1,8 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import MyField from '@/components/common/MyField.vue'
-import SearchDBTable from '@/components/tree/SearchDBTable.vue'
 
 const Prop = ref([
-	{
-		id: 0,
-		main: '',
-		label: 'Сервер приложений',
-		descr: 'Адрес сервера приложений, к которому подключается сервис Workflow',
-		select: true,
-		options: ['Адрес 1', 'Адрес 2', 'Адрес 3', 'Адрес 4', 'Адрес 5', 'Адрес 6'],
-	},
 	{
 		id: 2,
 		main: '',
@@ -66,9 +57,126 @@ const serverSelected = ref(false)
 const update = () => {
 	serverSelected.value = true
 }
+
+const address = ref('')
+const options = [
+	'Адрес 1',
+	'Адрес 2',
+	'Адрес 3',
+	'Адрес 4',
+	'Адрес 5',
+	'Адрес 6',
+]
+
+const connected = ref(false)
+const loading = ref(false)
+
+const connect = () => {
+	loading.value = true
+	setTimeout(() => {
+		connected.value = true
+		loading.value = false
+	}, 1000)
+}
+
+watch(address, val => {
+	if (val) {
+		connected.value = false
+	}
+})
+
+const bds = ref([
+	{
+		id: 0,
+		nam: 'AGSupport',
+		props: [
+			{
+				id: 0,
+				main: '',
+				label: 'Доля процессов',
+				descr:
+					'Маховик требует перейти к поступательно перемещающейся системе координат. ',
+			},
+			{
+				id: 1,
+				main: '',
+				label: 'Почтовое соединение',
+				descr: 'Маховик требует перейти. ',
+			},
+		],
+	},
+	{
+		id: 1,
+		nam: 'AGTest',
+		props: [
+			{
+				id: 0,
+				main: '',
+				label: 'Доля процессов',
+				descr:
+					'Маховик требует перейти к поступательно перемещающейся системе координат. ',
+			},
+			{
+				id: 1,
+				main: '',
+				label: 'Почтовое соединение',
+				descr: 'Маховик требует перейти. ',
+			},
+		],
+	},
+	{
+		id: 2,
+		nam: 'DBProd',
+		props: [
+			{
+				id: 0,
+				main: '',
+				label: 'Доля процессов',
+				descr:
+					'Маховик требует перейти к поступательно перемещающейся системе координат. ',
+			},
+			{
+				id: 1,
+				main: '',
+				label: 'Почтовое соединение',
+				descr: 'Маховик требует перейти. ',
+			},
+		],
+	},
+])
 </script>
 
 <template lang="pug">
+MyField(
+	v-model:main="address"
+	label='Сервер приложений'
+	descr='Адрес сервера приложений, к которому подключается сервис Workflow'
+	:select='true'
+	:options="options"
+	@update='update'
+)
+	q-btn.q-mt-sm(unelevated color="secondary" label="Connect" @click="connect" size="sm" :loading="loading" v-if='serverSelected')
+
+.data
+	label Обработка баз данных
+	.descr Здесь описание раздела. Уравнение малых колебаний, согласно третьему закону Ньютона, активно.
+
+	template(v-for="bd in bds" :key="bd.id" v-if='connected')
+		.bd
+			q-icon.q-mr-sm(name="mdi-database" color="secondary")
+			|{{ bd.nam }}
+		q-form
+			q-list
+				MyField(v-for="item in bd.props"
+					v-model:main="item.main"
+					:label='item.label'
+					:descr='item.descr'
+				)
+
+	.fu(v-else)
+		q-icon.q-mr-sm(name="mdi-alert" size='sm')
+		|Необходимо подключиться к серверу, чтобы настроить обработку БД.
+
 q-form
 	q-list
 		MyField(
@@ -78,13 +186,43 @@ q-form
 			:label="item.label" 
 			:descr="item.descr" 
 			:type="item.type"
-			:select='item.select'
-			:options='item.options'
-			@update='update'
 			)
-			div(v-if='serverSelected && item.id == 0')
-				AppServerWorkflow
+
 
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.q-input {
+	margin-top: 0;
+}
+.data {
+	padding: 1rem;
+	padding-top: 0.3rem;
+	margin-left: 1rem;
+	border: 1px solid transparent;
+	.descr {
+		color: #666;
+		font-size: 0.8rem;
+		font-weight: 400;
+	}
+	label {
+		font-size: 1rem;
+		font-weight: 600;
+		color: #666;
+	}
+}
+.block {
+	margin-left: 2rem;
+}
+.bd {
+	color: $secondary;
+	font-size: 1.1rem;
+	font-weight: 600;
+}
+.fu {
+	border: 1px solid #ccc;
+	padding: 1rem;
+	font-weight: 600;
+	color: $secondary;
+}
+</style>
