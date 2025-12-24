74de527ee6da5c62a7282218b530763188e700f9
Volkov Alexander <volkov.525@gmail.com>
2025-12-24 12:14:28 +0300

    prepare to search section

---
+++ src/components/setupcomponent/MasterSearch.vue
@@ -0,0 +1,230 @@
+<template lang="pug">
+q-dialog(v-model="modelValue" position="bottom" full-width persistent)
+	q-card.edit
+		div
+			q-card-section
+				.title
+					q-icon(name='mdi-wizard-hat')
+					span Подключение полнотекстового поиска
+
+			q-card-section.narrow
+				q-stepper(
+					v-model="step"
+					color="primary"
+					inactive-color="secondary"
+					done-color="teal"
+					alternative-labels
+					animated
+					flat
+					header-nav
+				)
+					q-step(
+						:name="1"
+						prefix="1"
+						title="Выбор БД"
+						:done="step > 1")
+
+						div дфоывдфлоывдло
+
+					q-step(
+						:name="2"
+						prefix="2"
+						title="Хранилища"
+						:done="step > 2")
+
+						div дфоывдфлоывдло
+
+					q-step(v-if='true'
+						:name="2"
+						prefix="2"
+						title="Внешняя база"
+						:done="step > 2")
+						div дфоывдфлоывдло
+
+
+					q-step(
+						:name="3"
+						prefix="3"
+						title="Языки"
+						:done="step > 3")
+						div дфоывдфлоывдло
+
+
+					q-step(
+						:name="4"
+						prefix="4"
+						title="Карточки"
+						:done="step > 4")
+						div дфоывдфлоывдло
+
+
+					q-step(
+						:name="5"
+						prefix="5"
+						title="Файлы"
+						:done="step > 5")
+						div дфоывдфлоывдло
+
+
+					q-step(
+						:name="6"
+						prefix="6"
+						title="Справочники"
+						:done="step > 6")
+						div дфоывдфлоывдло
+
+
+					q-step(
+						:name="7"
+						prefix="7"
+						title="Подтверждение параметров"
+						:done="step > 7")
+						div дфоывдфлоывдло
+
+
+		.bottom
+			q-separator
+			q-card-actions(v-if="ready" align="center")
+				q-btn(unelevated color="primary" @click="close" padding="xs xl") Готово
+
+			q-card-actions(v-else align="center")
+				q-btn(flat color="primary" @click="close").q-mr-xl Отмена
+
+				template(v-if="wiz.choose === 'create'")
+					q-btn(flat color="primary" @click="crBack") Назад
+					q-btn(unelevated color="primary" @click="crNext" padding="xs xl") Далее
+
+				template(v-if="wiz.choose === 'connect'")
+					q-btn(flat color="primary" @click="conBack") Назад
+					q-btn(unelevated color="primary" @click="conNext" padding="xs xl") Далее
+
+				template(v-if="wiz.choose === 'update'")
+					q-btn(flat color="primary" @click="upBack") Назад
+					q-btn(unelevated color="primary" @click="upNext" padding="xs xl") Далее
+		q-btn.close(flat round icon="mdi-close" color="primary" @click="close")
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import type { QTableProps } from 'quasar'
+import { useWiz } from '@/stores/wiz'
+import CreateDatabase from '@/components/setupcomponent/CreateDatabase.vue'
+import ConnectDatabase from '@/components/setupcomponent/ConnectDatabase.vue'
+import UpdateDatabase from '@/components/setupcomponent/UpdateDatabase.vue'
+
+const modelValue = defineModel<boolean>()
+
+const cr = ref()
+const con = ref()
+const up = ref()
+
+const step = ref(1)
+
+const wiz = useWiz()
+const emit = defineEmits(['update:modelValue'])
+
+const close = () => {
+	emit('update:modelValue', false)
+	wiz.choose = 'start'
+	wiz.done = false
+	wiz.finish = 0
+	wiz.setCreate(0)
+}
+const crNext = () => {
+	wiz.setCreate(0)
+	cr.value.nextStep()
+}
+const conNext = () => {
+	wiz.setCreate(0)
+	con.value.nextStep()
+}
+
+// const step = computed(() => {
+// 	if (wiz.choose === 'create') {
+// 		return cr.value?.step
+// 	}
+// 	if (wiz.choose === 'update') {
+// 		return up.value?.step
+// 	}
+// 	if (wiz.choose === 'connect') {
+// 		return con.value?.step
+// 	}
+// })
+
+const upNext = () => {
+	wiz.setCreate(0)
+	if (!!up.value) up.value.nextStep()
+}
+
+const crBack = () => {
+	cr.value.prevStep()
+}
+const conBack = () => {
+	con.value.prevStep()
+}
+const upBack = () => {
+	up.value.prevStep()
+}
+
+const ready = computed(() => {
+	if (wiz.choose === 'create' && step.value == 6) return true
+	else if (wiz.choose === 'update' && step.value == 4) return true
+	else if (wiz.choose === 'connect' && step.value == 6) return true
+	return false
+})
+</script>
+
+<style scoped lang="scss">
+.edit {
+	height: calc(100vh - 100px);
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	position: relative;
+	background: var(--bg-grey);
+}
+
+.title {
+	font-size: 1.5rem;
+	text-align: center;
+	color: $secondary;
+	vertical-align: baseline;
+
+	.q-icon {
+		margin-right: 0.5rem;
+		margin-top: -3px;
+		font-size: 1.6rem;
+	}
+}
+
+.close {
+	position: absolute;
+	top: 1rem;
+	right: 1rem;
+}
+
+.all {
+	width: 900px;
+	margin: 0 auto;
+}
+
+.q-tab-panels {
+	background: transparent;
+}
+
+.bottom {
+	background: white;
+}
+
+.q-table__card {
+	background: transparent;
+}
+
+:deep(.q-table tr.selected) {
+	background: var(--bg-selected);
+}
+// .narrow {
+// 	max-width: 1200px;
+// 	margin: 0 auto;
+// }
+</style>
--- src/components/tree/SearchConnect1.vue
+++ src/components/tree/SearchConnect1.vue
@@ -3,6 +3,7 @@ import { ref, computed, watch } from 'vue'
 import { useStore } from '@/stores/store'
 import type { QTableProps } from 'quasar'
 import SearchConnect from '@/components/tree/SearchConnect.vue'
+import MasterSearch from '@/components/setupcomponent/MasterSearch.vue'
 
 const store = useStore()
 const dialog = ref(false)
@@ -38,17 +39,32 @@ const columns: QTableProps['columns'] = [
 	{
 		name: 'server',
 		align: 'left',
-		label: 'Сервер приложений',
+		label: 'Сервер SQL',
 		field: 'server',
 		sortable: true,
 	},
 	{
-		name: 'index',
+		name: 'servertype',
 		align: 'left',
-		label: 'Индекс',
+		label: 'Тип сервера',
+		field: 'servertype',
+		sortable: true,
+	},
+	{
+		name: 'index',
+		align: 'center',
+		label: 'Индексирование',
 		field: 'index',
 		sortable: true,
 	},
+	{
+		name: 'indexplace',
+		align: 'left',
+		label: 'Расположение индексов',
+		field: 'indexplace',
+		sortable: true,
+	},
+	{ name: 'action1', align: 'right', label: '', field: '' },
 	{ name: 'action', align: 'right', label: '', field: '' },
 ]
 
@@ -70,7 +86,8 @@ const remove = (item: any) => {
 <template lang="pug">
 .database
 	br
-	label Сервер приложений
+	label БД с полнотекстовым поиском
+	.descr Базы данных, использующие сервис полнотекстового поиска
 	div
 		q-table.mywidth(:rows='rows' :columns='columns' row-key='name' hide-bottom)
 			template(v-slot:body-cell-active='props')
@@ -79,6 +96,7 @@ const remove = (item: any) => {
 						q-tooltip Доступна
 					q-icon(name="mdi-circle-slice-8" color="red" v-else)
 						q-tooltip Недоступна
+
 			template(v-slot:body-cell-action='props')
 				q-td.text-right
 					q-btn.q-mr-md(:props="props" flat icon='mdi-database-remove-outline' label="Отключить" size='sm' color="primary" dense @click.stop)
@@ -88,34 +106,45 @@ const remove = (item: any) => {
 									q-item-section Отключить
 					q-btn(:props="props" round flat icon='mdi-chevron-right' size='md' dense)
 
+			template(v-slot:body-cell-action1='props')
+				q-td.text-right
+					q-btn.q-mr-md(:props="props" flat icon='mdi-magnify-remove-outline' label="Удалить индексы" size='sm' color="primary" dense @click.stop)
+						q-menu
+							q-list(:props="props")
+								q-item(clickable :props="props" @click="remove(props.row)" v-close-popup).pink
+									q-item-section Отключить
+
+
 	q-btn(unelevated color='secondary' @click='dialog = !dialog' size="sm") Подключить
 
 br
 SearchConnect
 
-q-dialog(v-model="dialog")
-	q-card.q-pa-sm(style='min-width: 600px')
-		.row.items-center.q-pb-none.q-ml-md
-			.text-h6 Подключить БД
-			q-space
-			q-btn(icon="mdi-close" flat round dense v-close-popup)
-		q-card-section
-			div Выберите базы данных, для подключения
-			q-list
-				q-item(clickable tag='label' v-for="item in localDatabases" :key='item.psevdo')
-					q-item-section
-						.row.items-center
-							q-icon.q-mr-sm(name="mdi-database-outline" color="secondary" size='16px')
-							span {{ item.psevdo }}
-					q-item-section
-						.row.items-center
-							q-icon.q-mr-sm(name="mdi-server-network-outline" color="secondary" size='16px')
-							|{{ item.server }}
-					q-item-section(side)
-						q-checkbox(dense v-model="item.active" size="sm")
-		q-card-actions(align="right")
-			q-btn(flat label="Отмена" color="primary" v-close-popup)
-			q-btn(flat label="OK" color="primary" @click="applyChanges(); dialog = false")
+MasterSearch(v-model="dialog")
+
+// q-dialog(v-model="dialog")
+// 	q-card.q-pa-sm(style='min-width: 600px')
+// 		.row.items-center.q-pb-none.q-ml-md
+// 			.text-h6 Подключить БД
+// 			q-space
+// 			q-btn(icon="mdi-close" flat round dense v-close-popup)
+// 		q-card-section
+// 			div Выберите базы данных, для подключения
+// 			q-list
+// 				q-item(clickable tag='label' v-for="item in localDatabases" :key='item.psevdo')
+// 					q-item-section
+// 						.row.items-center
+// 							q-icon.q-mr-sm(name="mdi-database-outline" color="secondary" size='16px')
+// 							span {{ item.psevdo }}
+// 					q-item-section
+// 						.row.items-center
+// 							q-icon.q-mr-sm(name="mdi-server-network-outline" color="secondary" size='16px')
+// 							|{{ item.server }}
+// 					q-item-section(side)
+// 						q-checkbox(dense v-model="item.active" size="sm")
+// 		q-card-actions(align="right")
+// 			q-btn(flat label="Отмена" color="primary" v-close-popup)
+// 			q-btn(flat label="OK" color="primary" @click="applyChanges(); dialog = false")
 
 </template>
 
@@ -164,5 +193,8 @@ label {
 	display: inline-block;
 	max-width: unset; // отменяет 100% ширину по умолчанию
 	min-width: 600px;
+}
+.descr {
+	color: #666;
 }
 </style>
