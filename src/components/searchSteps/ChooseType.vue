19249010d68ee0dcf99a43c1e0d6633e1d275dda
Volkov Alexander <volkov.525@gmail.com>
2025-12-25 10:05:13 +0300

    add radio

--- src/components/searchSteps/ChooseType.vue
+++ src/components/searchSteps/ChooseType.vue
@@ -1,19 +1,5 @@
-<template>
-	<div class="q-gutter-md">
-		<q-btn
-			label="Путь A"
-			color="primary"
-			@click="select('A')"
-		/>
-		<q-btn
-			label="Путь B"
-			color="secondary"
-			@click="select('B')"
-		/>
-	</div>
-</template>
-
 <script setup lang="ts">
+import { ref } from 'vue'
 import type { FlowType } from '@/stores/useStepperStore'
 
 const emit = defineEmits<{
@@ -23,4 +9,36 @@ const emit = defineEmits<{
 function select(flow: FlowType) {
 	emit('select-flow', flow)
 }
+
+const group = ref(null)
+const options = [
+	{
+		label: 'Использовать внешний полнотекстовый поиск Elasticsearch',
+		value: 1,
+	},
+	{ label: 'Использовать локальную базу данных', value: 2 },
+]
 </script>
+
+<template lang="pug">
+	.text-bold Выберите хранилище
+
+	q-option-group(
+		:options="options"
+		type="radio"
+		v-model="group")
+
+
+	<!-- <div class="q-gutter-md"> -->
+	<!-- 	<q-btn -->
+	<!-- 		label="Путь A" -->
+	<!-- 		color="primary" -->
+	<!-- 		@click="select('A')" -->
+	<!-- 	/> -->
+	<!-- 	<q-btn -->
+	<!-- 		label="Путь B" -->
+	<!-- 		color="secondary" -->
+	<!-- 		@click="select('B')" -->
+	<!-- 	/> -->
+	<!-- </div> -->
+</template>
