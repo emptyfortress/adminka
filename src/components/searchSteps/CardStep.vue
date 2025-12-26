a2ba58a35049419a7562cee71a82303844fa578f
Volkov Alexander <volkov.525@gmail.com>
2025-12-26 14:06:28 +0300

    add cards tree

--- src/components/searchSteps/CardStep.vue
+++ src/components/searchSteps/CardStep.vue
@@ -1,5 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { cards } from '@/stores/cardsTree'
+
+const ticked = ref([])
+const expanded = ref([])
+</script>
+
 <template lang="pug">
 .all900
 	.arch
-		.text-bold Карточки
+		.text-bold Индексируемые типы карточек
+		q-tree(
+			:nodes='cards'
+			node-key='key'
+			tick-strategy="leaf"
+			v-model:ticked="ticked"
+      v-model:expanded="expanded"
+		)
 </template>
+
+<style scoped lang="scss">
+.all900 {
+	max-width: 600px;
+}
+</style>
