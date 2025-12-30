0ce7869313081bd08ebb9898825b49c391f5f4a9
Volkov Alexander <volkov.525@gmail.com>
2025-12-30 13:07:14 +0300

    wip

--- src/components/searchSteps/FacetStep.vue
+++ src/components/searchSteps/FacetStep.vue
@@ -1,9 +1,60 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, computed } from 'vue'
+import MyInput from '@/components/common/MyInput.vue'
+import { useStepperStore } from '@/stores/useStepperStore'
+
+const stepper = useStepperStore()
+
+const expanded = ref([])
+const filterRef = ref()
+const filter = ref()
+</script>
 
 <template lang="pug">
-.all900
+.sid
 	.arch
-		.text-bold Фасеты
+		.row.items-center.justify-between
+			.text-bold Дерево видов
+			MyInput(
+				ref="filterRef",
+				v-model="filter",
+				prependIcon='mdi-magnify'
+				clearable
+				noValidation
+			)
+		q-tree(
+			:nodes='facets'
+			node-key='key'
+			:filter="filter"
+			tick-strategy="leaf"
+			v-model:ticked="stepper.step4.cards"
+			v-model:expanded="expanded"
+		)
+	.arch
+		.text-bold Индексируемые типы карточек
+		// q-list(v-if="checkedItems.length")
+			q-item(v-for="(item, index) in checkedItems" :key="index" dense)
+				q-item-section(side)
+					q-icon(name="mdi-check" color="secondary" size='12px')
+				q-item-section
+					q-item-label {{ item }}
+		// .text-body2.q-mt-sm.text-grey(v-else) Нет выбранных элементов
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.sid {
+	width: 920px;
+	display: grid;
+	grid-template-columns: 600px 400px;
+	align-items: start;
+	column-gap: 1rem;
+	margin: 0 auto;
+}
+
+:deep(.q-checkbox--dense .q-checkbox__inner) {
+	width: 0.4em;
+	min-width: 0.4em;
+	height: 0.4em;
+	margin-right: 0.3rem;
+}
+</style>
