6fdb6e726b9f5d7e64b35e602ed7fcda1d2abd79
Volkov Alexander <volkov.525@gmail.com>
2026-01-20 11:39:12 +0300

    refactor: format code and add dense prop to q-item

--- src/components/searchSteps/FileStep.vue
+++ src/components/searchSteps/FileStep.vue
@@ -11,24 +11,24 @@ const filter = ref()
 const expanded = ref([])
 
 const options = [
-  {
-    label: 'Не индексировать',
-    value: 'notIndex',
-  },
-  { label: 'Индексировать оперативные файлы', value: 'use' },
-  { label: 'Индексировать оперативные и архивные файлы', value: 'use1' },
+	{
+		label: 'Не индексировать',
+		value: 'notIndex',
+	},
+	{ label: 'Индексировать оперативные файлы', value: 'use' },
+	{ label: 'Индексировать оперативные и архивные файлы', value: 'use1' },
 ]
 
 // Get the labels of all checked file types
 const checkedItems = computed(() => {
-  return stepper.step6.filetypes
-    .map(fileId => {
-      // Find the file by its label (now using label directly as identifier)
-      const foundFile = files.find(file => file.label === fileId)
-      return foundFile ? foundFile.label : null
-    })
-    .filter(Boolean) // Filter out any null values
-    .join(', ') // Join with commas
+	return stepper.step6.filetypes
+		.map(fileId => {
+			// Find the file by its label (now using label directly as identifier)
+			const foundFile = files.find(file => file.label === fileId)
+			return foundFile ? foundFile.label : null
+		})
+		.filter(Boolean) // Filter out any null values
+		.join(', ') // Join with commas
 })
 </script>
 
@@ -53,15 +53,13 @@ const checkedItems = computed(() => {
             clearable
             noValidation
           )
-        q-list(
-          bordered
-          separator
-        )
+        q-list
           q-item(
             v-for="file in files"
             :key="file.label"
             clickable
             v-ripple
+						dense
           )
             q-item-section(side)
               q-checkbox(
@@ -78,27 +76,27 @@ const checkedItems = computed(() => {
 
 <style scoped lang="scss">
 .grid {
-  display: grid;
-  grid-template-columns: 260px 1fr;
-  column-gap: 1rem;
-  row-gap: 0.5rem;
+	display: grid;
+	grid-template-columns: 260px 1fr;
+	column-gap: 1rem;
+	row-gap: 0.5rem;
 }
 .sid {
-  display: grid;
-  grid-template-columns: 410px 300px;
-  align-items: start;
-  column-gap: 0.5rem;
-  margin-top: 0.5rem;
+	display: grid;
+	grid-template-columns: 410px 300px;
+	align-items: start;
+	column-gap: 0.5rem;
+	margin-top: 0.5rem;
 }
 .all700 {
-  width: 720px;
-  margin: 0 auto;
+	width: 720px;
+	margin: 0 auto;
 }
 
 :deep(.q-checkbox--dense .q-checkbox__inner) {
-  width: 0.4em;
-  min-width: 0.4em;
-  height: 0.4em;
-  margin-right: 0.3rem;
+	width: 0.4em;
+	min-width: 0.4em;
+	height: 0.4em;
+	margin-right: 0.3rem;
 }
 </style>
