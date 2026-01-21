7eef99ff76aea71d75d53a635d4b3e78318f912f
Volkov Alexander <volkov.525@gmail.com>
2026-01-21 16:33:37 +0300

    fix label

--- src/components/searchSteps/ChooseType.vue
+++ src/components/searchSteps/ChooseType.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { ref, watch, computed } from 'vue'
 import { useStepperStore } from '@/stores/useStepperStore'
 import MyInput from '@/components/common/MyInput.vue'
 import MySelect from '@/components/common/MySelect.vue'
@@ -23,10 +23,10 @@ const options = [
 
 const options1 = [
 	{
-		label: 'Создавать внешнюю базу по умолчанию',
+		label: 'Создать БД',
 		value: 'create',
 	},
-	{ label: 'Использовать существующую базу', value: 'use' },
+	{ label: 'Использовать существующую БД', value: 'use' },
 ]
 
 const options2 = [
@@ -45,9 +45,9 @@ watch(
 )
 
 watch(
-	() => stepper.step2.db,
+	() => stepper.step2.externaldb,
 	() => {
-		if (stepper.step2.db == 'local') {
+		if (stepper.step2.externaldb == 'use') {
 			stepper.step2.servertype = 'MSSQL Server'
 			stepper.step2.servername = 'vega'
 			stepper.step2.checkvalid = 'SQLServer'
@@ -55,7 +55,7 @@ watch(
 			stepper.step2.pass = '***********'
 			stepper.step2.database = 'DvShowCase_Ft'
 		}
-		if (stepper.step2.db == 'external') {
+		if (stepper.step2.externaldb == 'create') {
 			stepper.step2.servertype = ''
 			stepper.step2.servername = ''
 			stepper.step2.checkvalid = ''
@@ -65,6 +65,10 @@ watch(
 		}
 	}
 )
+
+const dis = computed(() => {
+	return stepper.step2.externaldb == 'use' ? true : false
+})
 
 const ser = ['MSSQL Server', 'PostgreSQL']
 const che = ['SQL Server']
@@ -100,26 +104,43 @@ const che = ['SQL Server']
 				q-btn(unelevated color="secondary" label="Тест" size='sm')
 
 	transition(name='slide-top')
-		.arch(v-if='stepper.payload.flow == "A" && stepper.payload.db.length')
+		.arch(v-if='stepper.payload.flow == "A" && stepper.step2.db == "external"')
 			.grid
-				.text-bold Настройки:
+				.text-bold.q-mt-sm Внешняя БД:
+				
+				q-option-group(
+					:options="options1"
+					type="radio"
+					v-model="stepper.step2.externaldb")
+
+	transition(name='slide-top')
+		.arch(v-if='stepper.payload.flow == "A" && stepper.step2.db == "external" && !!stepper.step2.externaldb')
+			.grid
+				.text-bold Настройки подключения:
 				.grid2.q-ml-sm
 					label Тип сервера:
-					MySelect(v-model="stepper.step2.servertype" :options='ser')
+					MySelect(v-model="stepper.step2.servertype" :options='ser' :disable='dis')
 					label Имя сервера:
-					MyInput(v-model="stepper.step2.servername")
+					MyInput(v-model="stepper.step2.servername" :disable='dis')
 					label Проверка подлинности:
-					MySelect(v-model="stepper.step2.checkvalid" :options='che')
+					MySelect(v-model="stepper.step2.checkvalid" :options='che' :disable='dis')
 					label Логин:
-					MyInput(v-model="stepper.step2.login")
+					MyInput(v-model="stepper.step2.login" :disable='dis')
 					label Пароль:
-					MyInput(v-model="stepper.step2.pass")
+					MyInput(v-model="stepper.step2.pass" :disable='dis')
 					label База данных:
-					MyInput(v-model="stepper.step2.database")
+					MyInput(v-model="stepper.step2.database" :disable='dis')
 
 </template>
 
 <style lang="scss" scoped>
+:deep(.q-option-group) {
+	// display: flex;
+	// gap: 1rem;
+	display: grid;
+	grid-template-columns: 150px 1fr;
+	column-gap: 1rem;
+}
 .grid {
 	display: grid;
 	grid-template-columns: 250px 1fr;
