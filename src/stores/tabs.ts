6f40f7a457e2c239547bf028d5b8ea56550498b7
Volkov Alexander <volkov.525@gmail.com>
2026-01-27 09:29:09 +0300

    cont

--- src/stores/tabs.ts
+++ src/stores/tabs.ts
@@ -2,71 +2,81 @@ import { defineStore } from 'pinia'
 import { ref, computed } from 'vue'
 
 export const useTabs = defineStore('tabs', () => {
-  const tabs = ref([
-    {
-      id: 0,
-      field: 'prop',
-      name: 'prop',
-      label: 'Свойства',
-      modified: false,
-    },
-    {
-      id: 1,
-      field: 'control',
-      name: 'control',
-      label: 'Обслуживание',
-      modified: false,
-    },
-    {
-      id: 2,
-      field: 'outer',
-      name: 'outer',
-      label: 'Внешние хранилища',
-      modified: false,
-    },
-    {
-      id: 3,
-      field: 'arch',
-      name: 'arch',
-      label: 'Архивирование',
-      modified: false,
-    },
-    {
-      id: 5,
-      field: 'cache',
-      name: 'cache',
-      label: 'Кэширование',
-      modified: false,
-    },
-    {
-      id: 6,
-      field: 'log',
-      name: 'log',
-      label: 'Журналирование',
-      modified: false,
-    },
-    {
-      id: 8,
-      field: 'always',
-      name: 'always',
-      label: 'Распределение нагрузки',
-      modified: false,
-    },
-    {
-      id: 7,
-      field: 'module',
-      name: 'module',
-      label: 'Доп. настройки',
-      modified: false,
-    },
-  ])
+	const tabs = ref([
+		{
+			id: 0,
+			field: 'prop',
+			name: 'prop',
+			label: 'Свойства',
+			modified: false,
+		},
+		{
+			id: 1,
+			field: 'control',
+			name: 'control',
+			label: 'Обслуживание',
+			modified: false,
+		},
+		{
+			id: 2,
+			field: 'outer',
+			name: 'outer',
+			label: 'Внешние хранилища',
+			modified: false,
+		},
+		{
+			id: 3,
+			field: 'arch',
+			name: 'arch',
+			label: 'Архивирование',
+			modified: false,
+		},
+		{
+			id: 5,
+			field: 'cache',
+			name: 'cache',
+			label: 'Кэширование',
+			modified: false,
+		},
+		{
+			id: 6,
+			field: 'log',
+			name: 'log',
+			label: 'Журналирование',
+			modified: false,
+		},
+		{
+			id: 8,
+			field: 'always',
+			name: 'always',
+			label: 'Распределение нагрузки',
+			modified: false,
+		},
+		{
+			id: 7,
+			field: 'module',
+			name: 'module',
+			label: 'Доп. настройки',
+			modified: false,
+		},
+	])
 
-  const setTabMod = (n: number) => {
-    tabs.value[n].modified = true
-  }
+	const searchSections = ref([
+		{
+			id: 0,
+			field: 'commonProps',
+			name: 'commonProps',
+			label: 'Общие настройки',
+			modified: false,
+		},
+	])
 
-  return {
-    tabs,
-    setTabMod,
-  }
+	const setTabMod = (n: number) => {
+		tabs.value[n].modified = true
+	}
+
+	return {
+		tabs,
+		setTabMod,
+	}
 })
